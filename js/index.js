64658c1c89585fd167147e0386ab6ea88bc2fd08
simu6 <semantos678@gmail.com>
2022-05-11 06:22:56 +0600

    completed project section

--- js/index.js
+++ js/index.js
@@ -22,3 +22,27 @@ function hideLinks() {
   navLinks.classList.remove("active");
   toggleBtn.classList.remove("active");
 }
+
+// slider jquery 
+
+$(".carousel").owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsive: {
+    0:{
+      items:1,
+      nav: false
+    },
+    600:{
+      items:2,
+      nav: false
+    },
+    1000:{
+      items:3,
+      nav: false
+    }
+  }
+});
